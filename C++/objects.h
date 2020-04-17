baf3dd366261a4135a2c3b922a9f50998beabbd2
josegomz <josegomz.sa17@gmail.com>
2020-04-16 23:43:29 -0500

    fase inicial del juego
    creacion de la nave
    y un breve intro

---
+++ C++/objects.h
@@ -0,0 +1,17 @@
+
+//Objetos id
+enum IDS{JUGADOR, BALAS, ENEMIGOS};
+
+
+//jugador
+struct NaveEspacial{
+	int ID;
+	int x;
+	int y;
+	int vidas;
+	int velocidad;
+	int score;
+	int boundx;
+	int boundy;
+};
+
